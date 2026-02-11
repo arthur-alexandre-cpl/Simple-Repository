ee70f67c15c82bb9ad0767f9cf9078fa0a5dd934
arthur-alexandre-cpl <arthuralexandre50248@gmail.com>
2026-02-11 10:51:58 -0300

    Atualizar o AdvinheNumero.py

--- AdvinheNumero.py
+++ AdvinheNumero.py
@@ -4,8 +4,11 @@ numero = random.randint(1, 100)
 acertou = False
 tentativas = 0
 while acertou == False and tentativas < 10:
-    try:
+    try: 
         valor = int(input('Digite um número de 1 à 100:  '))
+        if valor < 1 or valor > 100:
+            print('Apenas números entre 1 e 100.')
+            continue
         tentativas += 1
         if valor > numero:
             print('Digite um número menor:  ')
